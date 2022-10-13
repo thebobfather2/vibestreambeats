9ff7c1a4e5f8a5127183ba543741761c94a6bfd3
Bryan Pike <bryanandrewpike@gmail.com>
2022-10-13 14:13:26 -0400

    box shadow on logo
    Signed-off-by: Bryan Pike <bryanandrewpike@gmail.com>

--- src/App.js
+++ src/App.js
@@ -24,7 +24,13 @@ function App() {
   return (
     <div className="App" style={{marginBottom: "0px"}}>
           <Navbar/>
-            <img style={{maxWidth: "300px", marginTop: "50px", marginBottom: "50px", borderRadius: "20px"}} src={logo} alt='vsn logo'/>
+            <img style={{
+              maxWidth: "300px", 
+              marginTop: "50px", 
+              marginBottom: "50px", 
+              borderRadius: "20px",
+              boxShadow: "0.2em 0.2em 0 0.2em rgba(0,0,0,0.17)",
+              }} src={logo} alt='vsn logo'/>
     <WalletCardEthers/>
     <div className='Body'>
     <img className='AudioPlayer' src={audioplayer} alt='audio player'/>
